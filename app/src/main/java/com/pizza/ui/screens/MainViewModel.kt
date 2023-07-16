4454142e1d83a0757631591c737bb2bd5cfa627f
ay0o5h <aayosh553@gmail.com>
2023-07-16 11:06:11 +0300

    refactore main view model

--- app/src/main/java/com/pizza/ui/screens/MainViewModel.kt
+++ app/src/main/java/com/pizza/ui/screens/MainViewModel.kt
@@ -2,8 +2,10 @@ package com.pizza.ui.screens
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.pizza.ui.screens.uiState.IngredientUIState
 import com.pizza.ui.screens.uiState.MainScreenUIState
 import com.pizza.ui.screens.uiState.PizzaSizesUIState
+import com.pizza.ui.screens.uiState.PizzaUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -22,24 +24,37 @@ class MainViewModel @Inject constructor() :ViewModel() {
             it.copy(selectedSize = pizzaSizesUIState)
         }
     }
+   private  fun updatePizzaIngredients(pizza: PizzaUIState, ingredientType: Ingredient,
+          toggleFunction: (IngredientUIState) -> IngredientUIState): PizzaUIState {
+        val updatedIngredients = pizza.ingredients.map { ingredient ->
+            if (ingredient.type == ingredientType) {
+                toggleFunction(ingredient)
+            } else {
+                ingredient
+            }
+        }
+        return pizza.copy(ingredients = updatedIngredients)
+    }
 
-    fun onSelectIngredient(ingredientType: Ingredient,currentPizza:Int) {
-        _state.update {
-            it.copy(
-                pizzas = it.pizzas.mapIndexed { index, pizza ->
-                    if (index == currentPizza) {
-                        pizza.copy(ingredients = pizza.ingredients.map { ingredient ->
-                            if (ingredient.type == ingredientType) {
-                                ingredient.copy(isSelected = !ingredient.isSelected)
-                            } else {
-                                ingredient
-                            }
-                        })
-                    } else {
-                        pizza
-                    }
-                }
-            )
+    private  fun updatePizzasList(pizzas: List<PizzaUIState>, currentPizza: Int, ingredientType: Ingredient,
+           toggleFunction: (IngredientUIState) -> IngredientUIState): List<PizzaUIState> {
+        return pizzas.mapIndexed { index, pizza ->
+            if (index == currentPizza) {
+                updatePizzaIngredients(pizza, ingredientType, toggleFunction)
+            } else {
+                pizza
+            }
         }
     }
+   private fun toggleIngredientSelection(ingredient: IngredientUIState): IngredientUIState {
+        return ingredient.copy(isSelected = !ingredient.isSelected)
+    }
+
+
+    fun onSelectIngredient(ingredientType: Ingredient, currentPizza: Int) {
+        _state.update {
+            it.copy(pizzas = updatePizzasList(it.pizzas, currentPizza, ingredientType, ::toggleIngredientSelection))
+        }
+    }
+
 }
