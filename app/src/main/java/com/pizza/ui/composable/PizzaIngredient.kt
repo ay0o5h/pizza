b2154baa9b4589e00122755b4ba44ec47d18a3aa
ay0o5h <aayosh553@gmail.com>
2023-07-16 11:18:09 +0300

    refactore pizza app

--- app/src/main/java/com/pizza/ui/composable/PizzaIngredient.kt
+++ app/src/main/java/com/pizza/ui/composable/PizzaIngredient.kt
@@ -1,48 +1,58 @@
 package com.pizza.ui.composable
 
-import android.annotation.SuppressLint
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import kotlin.random.Random
 
 
-@OptIn(ExperimentalAnimationApi::class)
+@OptIn(ExperimentalAnimationApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun PizzaIngredient(
     images : List<Int>,
     isSelected: Boolean =false,
     modifier: Modifier = Modifier,
+    pagerState: PagerState,
 ) {
         Box(modifier = modifier.size(250.dp)) {
-            androidx.compose.animation.AnimatedVisibility(
-                visible = isSelected,
-                enter = scaleIn(initialScale = 5f) ,
-                exit = scaleOut(),
-            ) {
-            images.forEach { image ->
-                val randomOffsetX = remember { mutableStateOf(Random.nextInt(80, 300 )) }
-                val randomOffsetY = remember { mutableStateOf(Random.nextInt(10, 210)) }
-                Image(
-                    painter = painterResource(id = image),
-                    contentDescription = null,
-                    modifier = Modifier.size(30.dp).offset(randomOffsetX.value.dp, randomOffsetY.value.dp)
-                )
-            }
+            if(pagerState.isScrollInProgress){
+                images.filter { isSelected }.forEach { image ->
+                    val randomOffsetX = remember { mutableStateOf(Random.nextInt(80, 300 )) }
+                    val randomOffsetY = remember { mutableStateOf(Random.nextInt(10, 210)) }
+                    Image(
+                        painter = painterResource(id = image),
+                        contentDescription = null,
+                        modifier = Modifier.size(30.dp).offset(randomOffsetX.value.dp, randomOffsetY.value.dp)
+                    )
+                }
+            }else{
+                androidx.compose.animation.AnimatedVisibility(
+                    visible = isSelected,
+                    enter = scaleIn(initialScale = 5f) ,
+                    exit = scaleOut(),
+                ) {
+                    images.forEach { image ->
+                        val randomOffsetX = remember { mutableStateOf(Random.nextInt(80, 300 )) }
+                        val randomOffsetY = remember { mutableStateOf(Random.nextInt(10, 210)) }
+                        Image(
+                            painter = painterResource(id = image),
+                            contentDescription = null,
+                            modifier = Modifier.size(30.dp).offset(randomOffsetX.value.dp, randomOffsetY.value.dp)
+                        )
+                    }
+                }
             }
         }
 }
--- app/src/main/java/com/pizza/ui/screens/MainScreen.kt
+++ app/src/main/java/com/pizza/ui/screens/MainScreen.kt
@@ -1,16 +1,10 @@
 package com.pizza.ui.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
-import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -18,11 +12,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
@@ -44,32 +36,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pizza.R
 import com.pizza.ui.composable.AddToCartButton
-import com.pizza.ui.composable.AnimateBackgroundAlignment
 import com.pizza.ui.composable.Appbar
 import com.pizza.ui.composable.PizzaIngredient
 import com.pizza.ui.screens.uiState.MainScreenUIState
 import com.pizza.ui.screens.uiState.PizzaSizesUIState
 import com.pizza.ui.theme.Typography
 import com.pizza.ui.theme.green
-import kotlin.random.Random
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -77,9 +65,16 @@ fun MainScreen(
     viewModel: MainViewModel = hiltViewModel(),
 ) {
     val state by viewModel.state.collectAsState()
-    val pagerState = rememberPagerState(
-
+    val pagerState = rememberPagerState(initialPage = 0)
+    val horizontalBias by animateFloatAsState(
+        targetValue =  when (state.selectedSize.type) {
+            "M" -> 0f
+            "L" -> .6f
+            else -> -.6f
+        },
+        animationSpec = tween(500)
     )
+ val alignment=remember { derivedStateOf { BiasAlignment(horizontalBias = horizontalBias, verticalBias = 0f) } }
     Scaffold(
         containerColor= Color.White,
         topBar = {
@@ -92,11 +87,12 @@ fun MainScreen(
             pagerState = pagerState,
             state=state,
             onSelectIngredient = { ingredient: Ingredient, index: Int -> viewModel.onSelectIngredient(ingredient, index) },
+            alignment = alignment.value
         )
     }
 }
 @SuppressLint("UnrememberedMutableState")
-@OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class,)
+@OptIn(ExperimentalFoundationApi::class, )
 @Composable
 fun MainContent(
     modifier: Modifier = Modifier,
@@ -104,14 +100,8 @@ fun MainContent(
     pagerState : PagerState,
     state: MainScreenUIState,
     onSelectIngredient: (Ingredient,Int) -> Unit,
+    alignment: BiasAlignment,
 ) {
-    val alignment by AnimateBackgroundAlignment(
-        when (state.selectedSize.type) {
-            "M" -> 0f
-            "L" -> .6f
-            else -> -.6f
-        }
-    )
     Column(modifier = modifier
         .fillMaxSize()
         .verticalScroll(rememberScrollState()),
@@ -134,7 +124,7 @@ fun MainContent(
             ) {
                 val animatedScale by animateFloatAsState(
                     targetValue =  state.selectedSize.number,
-                    animationSpec = tween(durationMillis = 200)
+                    animationSpec = tween(durationMillis = 300)
                 )
                 Box(modifier = Modifier
                     .fillMaxSize()
@@ -151,30 +141,22 @@ fun MainContent(
                         PizzaIngredient(
                             images =ingredient.ingredientImages,
                             isSelected = ingredient.isSelected,
+                            pagerState = pagerState,
                         )
                     }
                 }
             }
         }
-        Text(
-            stringResource(R.string._17),
-            style = Typography.titleLarge,
-            modifier = Modifier.padding(vertical = 16.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(56.dp)){
-            Card(
-                modifier = Modifier
-                    .size(56.dp)
-                    .align(alignment),
-                shape= CircleShape,
+        Text( stringResource(R.string._17), style = Typography.titleLarge, modifier = Modifier.padding(vertical = 16.dp))
+        Box( modifier = Modifier.fillMaxWidth().height(56.dp)){
+
+            Card(modifier = Modifier.size(56.dp).align(alignment), shape= CircleShape,
                 colors=CardDefaults.cardColors(
                     containerColor = Color.White,
-
                 ),
                 elevation = CardDefaults.elevatedCardElevation(10.dp),
             ){}
+
             Row(horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
